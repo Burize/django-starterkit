def3a057551d52dbd80bffd89f0821b832c77ed2
burize <varimartas@yandex.ru>
2021-09-05 20:44:49 +0300

    take creating initial data from migration to separate script

--- template/settings/base.py
+++ template/settings/base.py
@@ -18,6 +18,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.staticfiles',
+    'django_extensions',
     'ajax_select',
     'authentication',
     'market',
